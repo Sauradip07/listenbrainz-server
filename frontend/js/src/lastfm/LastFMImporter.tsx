2133812f66368e1cf78d76b52b0671d79b89e069
Kartik Ohri <kartikohri13@gmail.com>
2021-12-28 21:57:13 +0530

    add submit button

--- frontend/js/src/lastfm/LastFMImporter.tsx
+++ frontend/js/src/lastfm/LastFMImporter.tsx
@@ -668,6 +668,9 @@ export default class LastFmImporter extends React.Component<
             type="file"
             onSubmit={this.handleSpotifyImport}
           />
+          <button className="btn btn-success" type="submit">
+            Import Now!
+          </button>
         </form>
       </div>
     );
